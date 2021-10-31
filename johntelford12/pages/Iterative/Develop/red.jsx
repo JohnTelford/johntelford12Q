c27bdae1269c7892b8ae90da2b7dc120a0f1a0ad
John <john@johntelford.com>
2021-10-31 10:41:50 -0700

    box test
    Signed-off-by: John <john@johntelford.com>

--- johntelford12/pages/Iterative/Develop/red.jsx
+++ johntelford12/pages/Iterative/Develop/red.jsx
@@ -1,6 +1,13 @@
 
-export const Red = () => (
-    <div style={{padding: '15px', backgroundColor: 'red'}} >
-        <p>This is a JSX function</p>
+export const Red = (message) => (
+    <div style={{
+        padding: '15px', 
+        backgroundColor: 'RED',
+        color: "white"
+    }} >
+        <p>
+            Danger
+            watchout there be dragons
+        </p>
     </div>)
     export default Red;

538ef9405a9c87d2419d261066019f303cbb5660
John <john@johntelford.com>
2021-10-31 23:53:45 -0700

    chronosync <-> IA
    Signed-off-by: John <john@johntelford.com>

--- johntelford12/components/header.js
+++ johntelford12/components/header.js
@@ -46,7 +46,7 @@ export default function Header() {
             { title: "Develop", route: "/Iterative/Develop/develop" },
             { title: "Deploy", route: "/Iterative/Deploy/deploy"},
             { title: "Maintain", route: "/Iterative/Maintain/maintain" },
-            { title: "Company", route: "/Company/company" },
+            { title: "Company", route: "/Iterative/Company/company" },
           ].map(({ route, title }) => (
             <li className="mt-3 md:mt-0 md:ml-6" key={title}>
               <Link href={route}>
